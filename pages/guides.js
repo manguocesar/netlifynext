af1db031d17c3240837143650431fa3c81cbfea4
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-10-07 23:27:30 +0800

    text tototo

--- pages/guides.js
+++ pages/guides.js
@@ -3,7 +3,7 @@ import styles from '../styles/Guides.module.css'
 export default function Guides() {
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      <h2>All toto Guides</h2>
     </div> 
   )
 }
