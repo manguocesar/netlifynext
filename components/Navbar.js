aab3a644c53955dd72c02945c95e0ef65317403a
cesarcoderbunker <cesar.hertz@coderbunker.com>
2021-10-10 08:48:34 +0800

    auth

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,36 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
+  const { user, login } = React.useContext(AuthContext);
+  console.log("user", user, login);
+
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/guides">
+              <a>Guides</a>
+            </Link>
+          </li>
+          <li onClick={login} className="btn">
+            Login/Signup
+          </li>
         </ul>
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
       </div>
     </div>
-  )
+  );
 }
---
+++ stores/authContext.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, createContext } from "react";
+import netlifyIdentity from "netlify-identity-widget";
+
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false,
+});
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // on login
+    netlifyIdentity.on("login", (user) => {
+      setUser(user);
+      netlifyIdentity.close();
+    });
+
+    // on logout
+    netlifyIdentity.on("logout", (user) => {
+      setUser(null);
+    });
+
+    // connect with Netlify Identity
+    netlifyIdentity.init();
+  }, []);
+
+  const login = () => {
+    netlifyIdentity.open();
+  };
+
+  const logout = () => {
+    netlifyIdentity.logout();
+  };
+
+  const context = {
+    login,
+    logout,
+    user,
+  };
+
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
